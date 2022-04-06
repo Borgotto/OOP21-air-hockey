9bd05b8cd8ab4fa56e075e59e3d1e5afd5cca4d5
Emanuele Borghini <manuborg2002@gmail.com>
2022-04-06 09:22:35 +0200

    Lay out a basic gui for the main menu

--- src/gui/MenuGui.java
+++ src/gui/MenuGui.java
@@ -1,20 +1,40 @@
 package gui;
 
 import java.awt.*;
-import javax.swing.*; 
+import java.util.*;
+import java.util.List;
+
+import javax.swing.*;
 
 
-public class MenuGui extends JFrame{
+public class MenuGui extends JFrame {
     private static final long serialVersionUID = 1L;
+    
+    private List<JButton> buttons = new ArrayList<>();
 
     public MenuGui(int size) {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100*size, 100*size);
+        this.setTitle("Air Hockey");
         
-        JPanel grid = new JPanel(new GridLayout(size,size));
-        this.getContentPane().add(BorderLayout.CENTER,grid);
+        JPanel panel = new JPanel();
+        BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        panel.setLayout(layout);
+        this.getContentPane().add(panel);
         
-       
+        buttons.add(new JButton("New game"));
+        buttons.add(new JButton("Continue"));
+        buttons.get(buttons.size()-1).setEnabled(false);
+        buttons.add(new JButton("Settings"));
+        buttons.add(new JButton("Credits"));
+        buttons.add(new JButton("Quit"));
+        
+        buttons.forEach(button -> {
+            //button.setBorder(BorderFactory.createEmptyBorder(25, 25, 20, 20));
+            //button.setAlignmentX(Component.CENTER_ALIGNMENT);
+            //button.setAlignmentY(Component.CENTER_ALIGNMENT);
+            panel.add(button);
+        });
         
         this.setVisible(true);
     }
