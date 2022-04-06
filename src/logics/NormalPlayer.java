3c149a52f6303121f5a3df0339df98992ea9892d
Edoardo La Greca <elagreca@protonmail.com>
2022-04-06 09:52:13 +0200

    Add NormalPlayer class

---
+++ src/logics/NormalPlayer.java
@@ -0,0 +1,16 @@
+package logics;
+
+/**
+ * The class NormalPlayer represents a user-controlled player.
+ */
+public class NormalPlayer extends Player {
+    /**
+     * This is the constructor for NormalPlayer. It behaves pretty much the same as its superclass.
+     * @param posX The player's position on the X-axis.
+     * @param posY The player's position on the Y-axis.
+     * @param name The player's name.
+     */
+    public NormalPlayer(int posX, int posY, String name) {
+        super(posX, posY, name);
+    }
+}
