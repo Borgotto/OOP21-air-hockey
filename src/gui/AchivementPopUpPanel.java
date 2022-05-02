0e06a7fa5213ca040e3e4bfdc6ab9984b13f85d1
Francesca <francesca.lanzi6@studio.unibo.it>
2022-05-02 17:00:57 +0200

    Add AchivementPopUpPanel to gui package

---
+++ src/gui/AchivementPopUpPanel.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class AchivementPopUpPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	public AchivementPopUpPanel() {
+		var achivementMessage = new JPanel();
+		achivementMessage.setLayout(new BoxLayout(achivementMessage, BoxLayout.Y_AXIS));
+		
+	}
+}
