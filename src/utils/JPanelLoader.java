2ea578db46fb76dcb0f928ad7f3a7c71d5f6814b
Edoardo La Greca <elagreca@protonmail.com>
2022-05-06 16:30:07 +0200

    Add getParentFrame static method

--- src/utils/JPanelLoader.java
+++ src/utils/JPanelLoader.java
@@ -3,6 +3,10 @@ package utils;
 import javax.swing.*;
 
 public class JPanelLoader {
+
+    public static JFrame getParentFrame(JComponent component) {
+        return (JFrame) SwingUtilities.getWindowAncestor(component);
+    }
     
     public static void load(JFrame frame, JPanel panel) {
         frame.getContentPane().removeAll();
