cde2f16c67722600ca6dfa2a1bf1cc8c5b1b9642
Francesca <francesca.lanzi6@studio.unibo.it>
2022-05-06 16:26:17 +0200

    Create Achivement class in logics and update AchivementPopUpPanel

--- src/gui/AchivementPopUpPanel.java
+++ src/gui/AchivementPopUpPanel.java
@@ -1,13 +1,33 @@
 package gui;
 
+import java.awt.*;
+import java.awt.image.*;
 import javax.swing.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 
 public class AchivementPopUpPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	
+	private static final String IMAGE_PATH = "res/achivement.png";
+
 	public AchivementPopUpPanel() {
-		var achivementMessage = new JPanel();
-		achivementMessage.setLayout(new BoxLayout(achivementMessage, BoxLayout.Y_AXIS));
+		this.setName("Achivement!");
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		c.fill = GridBagConstraints.BOTH;
+
+		c.gridx = 0;
+		try {
+			BufferedImage img = ImageIO.read(new File(IMAGE_PATH));
+			ImageIcon icon = new ImageIcon(img);
+			JLabel label = new JLabel(icon);
+			JOptionPane.showMessageDialog(null, label);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		c.gridx = 1;
+		var achivementMessage = new JLabel();
 	}
 }
---
+++ src/logics/Achivement.java
@@ -0,0 +1,4 @@
+package logics;
+
+public class Achivement {
+}
