67db7ff719350a925914a613379b7a925ff9a1f3
Zettaiki <psebastian2001@hotmail.com>
2022-05-14 16:58:12 +0200

    Implement new physics with JBox2D library and update the game physics

---
+++ src/physics/Physics2D.java
@@ -0,0 +1,31 @@
+package physics;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+
+public class Physics2D {
+    
+    private Vec2 gravity = new Vec2(0.0f , 0.0f);
+    private World world = new World(gravity);
+    
+    private float physicsTime = 0.0f;
+    private float physicsTimeStep = 1.0f / 60.0f;
+    private int velocityIterations = 8;
+    private int positionIterations = 3;
+    
+    public void update(float dt) {
+        physicsTime += dt;
+        if (physicsTime >= 0.0f) {
+            physicsTime -= physicsTimeStep;
+            world.step(physicsTime, velocityIterations, positionIterations);
+        }
+    }
+
+    /**
+     * @return the world
+     */
+    public World getWorld() {
+        return world;
+    }
+    
+}
