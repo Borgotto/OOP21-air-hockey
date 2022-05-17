8f6c94fbb7b91ea10f1750f785188e4caf919c15
Zettaiki <psebastian2001@hotmail.com>
2022-05-17 13:38:30 +0200

    Update world time flow, implement getBodyPos.

--- src/physics/Physics2D.java
+++ src/physics/Physics2D.java
@@ -3,23 +3,45 @@ package physics;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
 
+import utils.Pair;
+
 public class Physics2D {
     
-    private Vec2 gravity = new Vec2(0.0f , 0.0f);
+    private final Vec2 gravity = new Vec2(0.0f , 0.0f);
     private World world = new World(gravity);
     
-    private float physicsTime = 0.0f;
-    private float physicsTimeStep = 1.0f / 60.0f;
-    private int velocityIterations = 8;
-    private int positionIterations = 3;
+    private final float physicsTime = 1.0f / 60.0f;
+    private final int velocityIterations = 6;
+    private final int positionIterations = 3;
     
-    public void update(float dt) {
-        physicsTime += dt;
-        if (physicsTime >= 0.0f) {
-            physicsTime -= physicsTimeStep;
-            world.step(physicsTime, velocityIterations, positionIterations);
-        }
+    public Physics2D() {
+    	
     }
+    
+    /**
+     * Update the physics world.
+     * @param dt the 
+     */
+    
+    public void update() {
+    	world.step(physicsTime, velocityIterations, positionIterations);  
+    }
+    
+    /**
+     * @param gameBody the RigidBody to search.
+     * @return pairPos the position in pixels.
+     */
+    public Pair<Double,Double> getBodyPos(RigidBody gameBody) {
+    	Vec2 vecPos = gameBody.getPosition();
+    	
+    	/**
+    	 * TODO: Implement unit converter between pixels and vectors.
+    	 */
+    	
+    	Pair<Double,Double> pairPos = new Pair<Double,Double>((double)vecPos.x,(double)vecPos.y);
+    	return pairPos;
+    }
+    
 
     /**
      * @return the world
