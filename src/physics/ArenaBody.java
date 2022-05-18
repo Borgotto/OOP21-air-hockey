0d256ff6131dcbf7c50c9915740447aa07f1ea3e
Zettaiki <psebastian2001@hotmail.com>
2022-05-18 17:00:56 +0200

    Updated builder with size of the arena

--- src/physics/ArenaBody.java
+++ src/physics/ArenaBody.java
@@ -1,3 +1,4 @@
+
 package physics;
 
 import org.jbox2d.collision.shapes.EdgeShape;
@@ -7,13 +8,13 @@ import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 
-public class ArenaBody extends RigidBody {
+public class ArenaBody extends RigidBodyImpl {
 	
 	/**
 	 * Arena body generator
 	 * @param world
 	 */
-    public ArenaBody(World world) {
+    public ArenaBody(float width, float height, World world) {
         setBodyTypeDef(BodyType.STATIC);
         Body arenaBody = world.createBody(getBodyDef());
         
@@ -27,18 +28,18 @@ public class ArenaBody extends RigidBody {
         //The arena dimensions are 16x32 meters. (Proportion: 9:16)
                 
         // Vertical walls of the arena
-        wallShape.set(new Vec2(0.0f, 0.0f), new Vec2(0.0f, 32.0f));
+        wallShape.set(new Vec2(0.0f, 0.0f), new Vec2(0.0f, height));
         arenaBody.createFixture(sd);
         // Right wall
-        wallShape.set(new Vec2(18.0f, 0.0f), new Vec2(18.0f, 32.0f));
+        wallShape.set(new Vec2(width, 0.0f), new Vec2(width, height));
         arenaBody.createFixture(sd);
         
         // Horizontal walls of the arena
         // Bottom wall
-        wallShape.set(new Vec2(0.0f, 0.0f), new Vec2(18.0f, 0.0f));
+        wallShape.set(new Vec2(0.0f, 0.0f), new Vec2(width, 0.0f));
         arenaBody.createFixture(sd);
         // Top Wall
-        wallShape.set(new Vec2(0.0f, 32.0f), new Vec2(18.0f, 32.0f));
+        wallShape.set(new Vec2(0.0f, height), new Vec2(width, height));
         arenaBody.createFixture(sd);
         
         setBody(arenaBody);
