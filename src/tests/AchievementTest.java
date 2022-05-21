f1c6814f6a04511ecb7d2fca771d568ab3f292bd
Francesca Lanzi <francesca.lanzi6@studio.unibo.it>
2022-05-21 18:16:22 +0200

    Add test for Achievement class

---
+++ src/tests/AchievementTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import logics.AchievementImpl;
+import logics.GameState;
+
+class AchievementTest {
+
+	@Test
+	void testAchievement() {
+		var gamestate = new GameState();
+        var achievement = new AchievementImpl(null, "Champion", "Score 5 goals in one game", (game) -> game.getMainPlayer().getScore() >= 5);
+        var achievement2 = new AchievementImpl(null, "PogChamp", "score 15 goals in one game", (game) -> game.getMainPlayer().getScore() >= 15);
+        
+        System.out.println("initial score: "+gamestate.getMainPlayer().getScore());
+        
+        for(int i=0;i<10;i++) {
+        	System.out.println("\n\nscored!");
+        	gamestate.getMainPlayer().scorePoint();
+        	System.out.println("score: "+gamestate.getMainPlayer().getScore());
+        }
+        assertTrue(achievement.isUnlocked(gamestate));
+        assertFalse(achievement2.isUnlocked(gamestate));
+	}
+
+}
