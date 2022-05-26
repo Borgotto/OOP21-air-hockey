1146e037e8ae0e3c8e46b4b63c2490767ff57a31
Edoardo La Greca <elagreca@protonmail.com>
2022-05-26 19:16:42 +0200

    Add ActionListeners for the restart and exit buttons

--- src/gui/PausePanel.java
+++ src/gui/PausePanel.java
@@ -1,6 +1,8 @@
 package gui;
 
+import logics.GameState;
 import utils.JPanelLoader;
+import utils.MessageDialogBuilder;
 
 import javax.swing.*;
 import java.awt.*;
@@ -27,11 +29,21 @@ public class PausePanel extends AbstractGridBagLayoutJPanel {
         c.gridy = 2;
         c.anchor = GridBagConstraints.CENTER;
         JButton restart = new JButton("Restart");
+        restart.addActionListener(e -> {
+            JPanelLoader.load(JPanelLoader.getParentFrame(this), new GamePanel(new GameState()));
+        });
         this.add(restart, c);
 
         c.gridy = 3;
         c.anchor = GridBagConstraints.NORTH;
         JButton exit = new JButton("Exit");
+        exit.addActionListener(e -> {
+            int answer = JOptionPane.showConfirmDialog(null, "Exit?", "Confirm exit?", JOptionPane.YES_NO_OPTION);
+            if (answer == JOptionPane.YES_OPTION) {
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
+                frame.dispose();
+            }
+        });
         this.add(exit, c);
     }
 }
