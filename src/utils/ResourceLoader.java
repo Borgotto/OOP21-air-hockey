708c9199e16418ff595716ca769828514f0fad5d
Edoardo La Greca <elagreca@protonmail.com>
2022-05-27 17:14:52 +0200

    Add ResourceLoader class

---
+++ src/utils/ResourceLoader.java
@@ -0,0 +1,30 @@
+package utils;
+
+import gui.ExceptionPanel;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+/**
+ * Load resources from the JAR file.
+ */
+public class ResourceLoader {
+    /**
+     * Load an image from the JAR file.
+     * @param resourceName The resource name (without path).
+     * @return The image as a BufferedImage.
+     */
+    public static BufferedImage loadImage(String resourceName) {
+        BufferedImage img;
+
+        try {
+            img = ImageIO.read(new FileInputStream("res/" + resourceName));
+        } catch (IOException e) {
+            img = new BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB);
+            new ExceptionPanel(e);
+        }
+
+        return img;
+    }
+}
