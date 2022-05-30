74f0898cda30a034c68c6d64f0e6428a118afeec
Pablo Vargas <psebastian2001@hotmail.com>
2022-05-30 13:45:38 +0200

    Update UnitConverter with delta parameter

--- src/utils/UnitConverter.java
+++ src/utils/UnitConverter.java
@@ -3,15 +3,21 @@ package utils;
 import org.jbox2d.common.Vec2;
 
 public class UnitConverter {
-
-	public static Pair<Int,Int> MeterToPixel(final Vec2 posVector, final double delta) {
+    
+    private static final const double deltaConversion;
+    
+    public UnitConverter(double deltaConversion) {
+        this.deltaConversion = deltaConversion;
+    }
+    
+	public static Pair<Integer,Integer> MeterToPixel(final Vec2 posVector, final double delta) {
 		int xPair = (int)(posVector.x*delta);
 		int yPair = (int)(posVector.y*delta);
-		Pair<Int,Int> posPair = new Pair<Int,Int>(xPair, yPair);
+		Pair<Integer,Integer> posPair = new Pair<Integer,Integer>(xPair, yPair);
 		return posPair;
 	}
 	
-	public static Vec2 PixelToMeter(final Pair<Int,Int> posPair, final double delta) {
+	public static Vec2 PixelToMeter(final Pair<Integer,Integer> posPair, final double delta) {
 		float xVec = (float)(posPair.getX()/delta);
 		float yVec = (float)(posPair.getY()/delta);
 		Vec2 posVector = new Vec2(xVec, yVec);
