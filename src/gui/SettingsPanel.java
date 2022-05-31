482ccaa464a01c74c14346cc4cb31f7a875e8893
Emanuele Borghini <manuborg2002@gmail.com>
2022-05-31 10:20:09 +0200

    Add Dimension to SettingsPanel

--- src/gui/SettingsPanel.java
+++ src/gui/SettingsPanel.java
@@ -3,13 +3,14 @@ package gui;
 import utils.JPanelLoader;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class SettingsPanel extends AbstractGridBagLayoutJPanel {
     private static final long serialVersionUID = 1L;
     
     public SettingsPanel() {
-        super("Air Hockey - Settings");
+        super("Air Hockey - Settings", new Dimension(GUI.getMinScreenSize(), GUI.getMinScreenSize()));
         
         var b1 = new JButton("Go back");
         b1.addActionListener(e -> {
