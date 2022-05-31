49dedeaf81ed327dc232a47314eff3514f95d1e0
Edoardo La Greca <elagreca@protonmail.com>
2022-05-31 14:40:11 +0200

    Make UnitConverter static

--- src/utils/UnitConverter.java
+++ src/utils/UnitConverter.java
@@ -4,32 +4,22 @@ import org.jbox2d.common.Vec2;
 
 public class UnitConverter {
    
-    private double conversionFactor;
-    
-    public UnitConverter() {
-        this.conversionFactor = 1.0;
-    }
-    
-    public UnitConverter(final double conversionFactor) {
-        this.conversionFactor = conversionFactor;
-    }
+    public static double conversionFactor;
 
-	public Pair<Integer,Integer> MeterToPixel(final Vec2 posVector) {
-		int xPair = (int)(posVector.x*this.conversionFactor);
-		int yPair = (int)(posVector.y*this.conversionFactor);
-		Pair<Integer,Integer> posPair = new Pair<Integer,Integer>(xPair, yPair);
-		return posPair;
+	public static Pair<Integer,Integer> MeterToPixel(final Vec2 posVector) {
+		int xPair = (int)(posVector.x * conversionFactor);
+		int yPair = (int)(posVector.y * conversionFactor);
+		return new Pair<Integer,Integer>(xPair, yPair);
 	}
 	
 	public Vec2 PixelToMeter(final Pair<Integer,Integer> posPair) {
-		float xVec = (float)(posPair.getX()/this.conversionFactor);
-		float yVec = (float)(posPair.getY()/this.conversionFactor);
-		Vec2 posVector = new Vec2(xVec, yVec);
-		return posVector;
+		float xVec = (float)(posPair.getX() / conversionFactor);
+		float yVec = (float)(posPair.getY() / conversionFactor);
+		return new Vec2(xVec, yVec);
 	}
 	
 	public void setConversionFactor(final double conversionFactor) {
-	    this.conversionFactor = conversionFactor;
+	    UnitConverter.conversionFactor = conversionFactor;
 	}
 	
 }
