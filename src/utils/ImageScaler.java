d91dcfb8f90201f6f75553eb5968fab46a94c267
Emanuele Borghini <manuborg2002@gmail.com>
2022-05-31 12:36:01 +0200

    rename scale method to fit and add new scale

--- src/utils/ImageScaler.java
+++ src/utils/ImageScaler.java
@@ -3,7 +3,25 @@ package utils;
 import java.awt.*;
 
 public class ImageScaler {
-    public static Image scale(Image img, Dimension dim) {
+    public static Image fit(Image img, Dimension dim) {
         return img.getScaledInstance((int) dim.getWidth(), (int) (dim.getHeight()), Image.SCALE_DEFAULT);
+    }
+
+    public static Image scale(Image img, Dimension bounds) {
+        Dimension imgDim = new Dimension(img.getWidth(null), img.getHeight(null));
+        Dimension newDim = new Dimension(imgDim.width, imgDim.height);
+
+        // Scale the image dimension to fit the bounds width
+        if (imgDim.getWidth() > bounds.getWidth()) {
+            newDim.setSize(bounds.getWidth(), (bounds.getWidth()*imgDim.getHeight())/imgDim.getWidth());
+        }
+
+        // Scale the image dimension to fit the bounds height
+        if (newDim.getHeight() > bounds.getHeight()) {
+            newDim.setSize((bounds.getHeight()*imgDim.getWidth())/imgDim.getHeight(), bounds.getHeight());
+        }
+
+        // fit the image to the new dimensions
+        return ImageScaler.fit(img, newDim);
     }
 }
