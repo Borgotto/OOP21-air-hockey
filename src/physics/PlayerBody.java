25e4da57573268903d5390430282f43cd04556cd
Pablo Vargas <psebastian2001@hotmail.com>
2022-05-31 11:03:01 +0200

    Add and implement PlayerBody interface

--- src/physics/PlayerBody.java
+++ src/physics/PlayerBody.java
@@ -1,50 +1,11 @@
 package physics;
 
-import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.FixtureDef;
 
-public class PlayerBody extends RigidBodyImpl {
-	
-	/**
-	 * Player body generator
-	 * @param radius
-	 * @param pos Position to be spawned
-	 * @param world
-	 */
-    public PlayerBody(final float radius, final Vec2 pos, final Physics2D physicsWorld) {
-    	setWorld(world);
-        setBodyTypeDef(BodyType.DYNAMIC);
-        setLinearDampingDef(0.0f);
-        setStartPositionDef(pos);
-        configBodyDef();
-        
-        CircleShape shape = new CircleShape();
-        shape.m_radius = radius;
-        
-        FixtureDef fixture = new FixtureDef();
-        fixture.shape = shape;
-        fixture.density = 0.4f;
-        fixture.friction = 0.3f;
-        
-        // Bit mask for mid arena fixture collision
-        fixture.filter.categoryBits = 0x0002;
-        
-        Body playerBody = physicsWorld.addRigidBody(this);
-        playerBody.createFixture(fixture);
-        setBody(playerBody);
-    }
+public interface PlayerBody extends RigidBody {
     
-    // Methods only for Enemy IA
+    public void updateSpeedVec(final Vec2 force);
     
-    /**
-     * Apply a force to the player body.
-     * @param force to be applied to the body.
-     */
-    public void updateSpeedVec(final Vec2 force) {
-        getBody().applyForceToCenter(force);
-    }
+    public float getRadius();
     
 }
---
+++ src/physics/PlayerBodyImpl.java
@@ -0,0 +1,61 @@
+package physics;
+
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+
+public class PlayerBodyImpl extends RigidBodyImpl implements PlayerBody {
+	
+    private final float radius;
+    
+	/**
+	 * Player body generator
+	 * @param radius
+	 * @param pos Position to be spawned
+	 * @param world
+	 */
+    public PlayerBodyImpl(final float radius, final Vec2 pos, final Physics2D physicsWorld) {
+    	this.radius = radius;
+    	
+        setWorld(world);
+        setBodyTypeDef(BodyType.DYNAMIC);
+        setLinearDampingDef(0.0f);
+        setStartPositionDef(pos);
+        configBodyDef();
+        
+        CircleShape shape = new CircleShape();
+        shape.m_radius = radius;
+        
+        FixtureDef fixture = new FixtureDef();
+        fixture.shape = shape;
+        fixture.density = 0.4f;
+        fixture.friction = 0.3f;
+        
+        // Bit mask for mid arena fixture collision
+        fixture.filter.categoryBits = 0x0002;
+        
+        Body playerBody = physicsWorld.addRigidBody(this);
+        playerBody.createFixture(fixture);
+        setBody(playerBody);
+    }
+
+    /**
+     * Apply a force to the player body.
+     * @param force to be applied to the body.
+     */
+    @Override
+    public void updateSpeedVec(final Vec2 force) {
+        getBody().applyForceToCenter(force);
+    }
+    
+    /**
+     * @return the radius
+     */
+    @Override
+    public float getRadius() {
+        return radius;
+    }
+    
+}
