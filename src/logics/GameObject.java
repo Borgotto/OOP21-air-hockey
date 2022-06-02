afe2dd7fa8253d9b7c5b9c7965a78993a8e80b52
Edoardo La Greca <elagreca@protonmail.com>
2022-06-02 18:09:39 +0200

    Change GameObject to extend Serializable interface

--- src/logics/GameObject.java
+++ src/logics/GameObject.java
@@ -1,12 +1,14 @@
 package logics;
 
 
+import java.io.Serializable;
+
 /**
  * interface that defines a game object that needs its state to be updated every
  * frame
  */
-public interface GameObject {
-    
+public interface GameObject extends Serializable {
+
     /**
      * Method that allows to update the {@code GameObject}
      */
