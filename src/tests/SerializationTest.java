c893fdb741748e3bfde88851f700a68e571f5fb7
Edoardo La Greca <elagreca@protonmail.com>
2022-06-02 18:11:35 +0200

    Simplify expressions using assertEquals instead of assertTrue + equals

--- src/tests/SerializationTest.java
+++ src/tests/SerializationTest.java
@@ -5,7 +5,7 @@ import utils.ObjectSerializer;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class SerializationTest {
 
@@ -15,33 +15,33 @@ public class SerializationTest {
     public void serializeArena() throws IOException, ClassNotFoundException {
         Arena arena = this.gameState.getArena();
         ObjectSerializer.serialize(arena, "test.ser");
-        assertTrue(ObjectSerializer.deserialize("test.ser").equals(arena)) ;
+        assertEquals(ObjectSerializer.deserialize("test.ser"), arena);
     }
 
     @org.junit.Test
     public void serializeMainPlayer() throws IOException, ClassNotFoundException {
         MainPlayer mainPlayer = this.gameState.getMainPlayer();
         ObjectSerializer.serialize(mainPlayer, "test.ser");
-        assertTrue(ObjectSerializer.deserialize("test.ser").equals(mainPlayer)) ;
+        assertEquals(ObjectSerializer.deserialize("test.ser"), mainPlayer);
     }
 
     @org.junit.Test
     public void serializeEnemyPlayer() throws IOException, ClassNotFoundException {
         EnemyPlayer enemyPlayer = this.gameState.getEnemyPlayer();
         ObjectSerializer.serialize(enemyPlayer, "test.ser");
-        assertTrue(ObjectSerializer.deserialize("test.ser").equals(enemyPlayer)) ;
+        assertEquals(ObjectSerializer.deserialize("test.ser"), enemyPlayer);
     }
 
     @org.junit.Test
     public void serializePuck() throws IOException, ClassNotFoundException {
         Puck puck = this.gameState.getPuck();
         ObjectSerializer.serialize(puck, "test.ser");
-        assertTrue(ObjectSerializer.deserialize("test.ser").equals(puck)) ;
+        assertEquals(ObjectSerializer.deserialize("test.ser"), puck);
     }
 
     @org.junit.Test
     public void serializeGameState() throws IOException, ClassNotFoundException {
         ObjectSerializer.serialize(this.gameState, "test.ser");
-        assertTrue(ObjectSerializer.deserialize("test.ser").equals(this.gameState)) ;
+        assertEquals(ObjectSerializer.deserialize("test.ser"), this.gameState);
     }
 }
