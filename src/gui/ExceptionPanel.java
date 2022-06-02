4f4132539cc43e8bc41edd5b4a610425bbcfa390
Edoardo La Greca <elagreca@protonmail.com>
2022-06-02 16:37:19 +0200

    Change error message from partial error to complete error

--- src/gui/ExceptionPanel.java
+++ src/gui/ExceptionPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class ExceptionPanel {
     public ExceptionPanel(Exception ex) {
-        JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(null, ex.toString(), "Error", JOptionPane.ERROR_MESSAGE);
 
         // Close program
         System.exit(1);
