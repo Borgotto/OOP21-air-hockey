64e7c37d6c15138184f4979d960c81dfb290cfc1
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-04 14:50:39 +0200

    Add constructor with message to ExceptionPanel

--- src/gui/ExceptionPanel.java
+++ src/gui/ExceptionPanel.java
@@ -4,9 +4,15 @@ import javax.swing.*;
 
 public class ExceptionPanel {
     public ExceptionPanel(Exception ex) {
-        ex.printStackTrace();
-        JOptionPane.showMessageDialog(null, ex.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+        this(null,ex);
+    }
 
+    public ExceptionPanel(String message, Exception ex) {
+        ex.printStackTrace();
+        if (message == null) {
+            message = ex.toString();
+        }
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
         System.exit(1);
     }
 }
