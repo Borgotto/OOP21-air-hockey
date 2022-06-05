fb13875ec2d19bf6c57bab926463f8ec33048286
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-05 13:04:56 +0200

    Create primitive Game Test

---
+++ src/tests/GameTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import logics.GameState;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    private GameState game;
+
+    @org.junit.Before
+    public void initTests() {
+        this.game = new GameState();
+
+        // render the first 1.000 frames (16.66 seconds) of the game
+        for(int i=0;i<1000;i++) {
+            this.game.update();
+        }
+    }
+
+    @org.junit.Test
+    public void testGoals() {
+        // check out the scores
+        assertEquals(0, (int) this.game.getMainPlayer().getScore());
+        assertEquals(0, (int) this.game.getEnemyPlayer().getScore());
+    }
+}
