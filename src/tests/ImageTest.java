c720e620534b67dcc3171e44592a3cf47c2ca86c
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-05 23:44:56 +0200

    Create ImageTest

---
+++ src/tests/ImageTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import utils.ImageModifier;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.assertEquals;
+
+public class ImageTest {
+
+    private BufferedImage originalImg;
+
+    @org.junit.Before
+    public void initTest() {
+        originalImg = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = originalImg.createGraphics();
+        g.setColor(Color.RED);
+    }
+
+    @org.junit.Test
+    public void testFit() {
+        Image img = ImageModifier.fit(originalImg, new Dimension(200, 200));
+        assertEquals(200, img.getWidth(null));
+        assertEquals(200, img.getHeight(null));
+    }
+
+    @org.junit.Test
+    public void testScale() {
+        Image img = ImageModifier.scale(originalImg, new Dimension(100, 200));
+        assertEquals(100, img.getWidth(null));
+        assertEquals(100, img.getHeight(null));
+    }
+
+    @org.junit.Test
+    public void testColor() {
+        assertEquals(Color.RED, new Color(originalImg.getRGB(0, 0)));
+        Image img = ImageModifier.color(originalImg, Color.BLUE);
+        BufferedImage newImg = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = newImg.createGraphics();
+        graphics.drawImage(img, 0, 0, null);
+        assertEquals(Color.BLUE, new Color(newImg.getRGB(0, 0)));
+    }
+}
