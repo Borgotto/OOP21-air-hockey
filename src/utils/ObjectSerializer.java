ed970d1f1eeb7c4bf78ee575f41bae9606b6396e
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-05 13:16:14 +0200

    Change method used to create folders

--- src/utils/ObjectSerializer.java
+++ src/utils/ObjectSerializer.java
@@ -1,15 +1,15 @@
 package utils;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
 
 /**
  * Functional interface to serialize and deserialize a generic object {@code O}
  */
 public interface ObjectSerializer <O> {
     static <O> void serialize(O obj, Path filePath) throws IOException {
-        new File(filePath.getParent().toString()).mkdirs(); // create directory if needed
+        Files.createDirectories(filePath.getParent()); // create directory if needed
         try (FileOutputStream file = new FileOutputStream(filePath.toString());
                 ObjectOutputStream out = new ObjectOutputStream(file);) {
             out.writeObject(obj);
