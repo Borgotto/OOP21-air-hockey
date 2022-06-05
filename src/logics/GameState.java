db776d66f6689e197d1044551828123c2bef0156
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-05 15:31:57 +0200

    Implement load method

--- src/logics/GameState.java
+++ src/logics/GameState.java
@@ -18,17 +18,18 @@ public class GameState implements GameObject, Serializable {
 
     public static final Path savePath = Path.of(System.getProperty("user.dir") + "/config/save/save.ser");
 
-    private final transient Physics2D gamePhysics = new Physics2DImpl();
-    private final Arena arena;
+    private final transient Physics2D gamePhysics;
     private final MainPlayer mainPlayer;
     private final EnemyPlayer enemyPlayer;
     private final Puck puck;
-    private final Integer maxScore;
+    private final Arena arena;
+    private Integer maxScore;
 
     /**
      * Create a new GameState object using default values.
      */
     public GameState() {
+        this.gamePhysics = new Physics2DImpl();
         this.arena = new ArenaImpl(50.0f, this.gamePhysics);
         this.maxScore = 35;
 
@@ -86,7 +87,25 @@ public class GameState implements GameObject, Serializable {
         /*TODO*/
     }
 
+    /**
+     * Save the current {@link GameState} to a serialized file.
+     * @throws IOException
+     */
     public void save() throws IOException {
-        ObjectSerializer.serialize(this, this.savePath);
+        ObjectSerializer.serialize(this, GameState.savePath);
+    }
+
+    /**
+     * Loads the game state from a serialized save file.
+     * @throws IOException, ClassNotFoundException
+     */
+    public void load() throws IOException, ClassNotFoundException {
+        // Load the game state from the save file
+        GameState savedGame = ObjectSerializer.deserialize(GameState.savePath);
+        // Set the current game state to the saved game state
+        this.arena.setGoalWidth(savedGame.getArena().getGoalWidth());
+        this.mainPlayer.setScore(savedGame.mainPlayer.getScore());
+        this.enemyPlayer.setScore(savedGame.enemyPlayer.getScore());
+        this.maxScore = savedGame.maxScore;
     }
 }
