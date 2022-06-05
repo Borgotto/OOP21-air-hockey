182c3d2518cc95a02c60b36ec8a292293e944405
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-05 17:32:42 +0200

    Fix typo in gameState var

--- src/tests/AchievementTest.java
+++ src/tests/AchievementTest.java
@@ -9,19 +9,19 @@ public class AchievementTest {
 
     @org.junit.Test
 	public void testAchievement() {
-		var gamestate = new GameStateImpl();
+		var gameState = new GameStateImpl();
         var achievement = new AchievementImpl("", "Champion", "Score 5 goals in one game", (game) -> game.getMainPlayer().getScore() >= 5);
         var achievement2 = new AchievementImpl("", "PogChamp", "score 15 goals in one game", (game) -> game.getMainPlayer().getScore() >= 15);
         
-        System.out.println("initial score: "+gamestate.getMainPlayer().getScore());
+        System.out.println("initial score: "+gameState.getMainPlayer().getScore());
         
         for(int i=0;i<10;i++) {
         	System.out.println("\n\nscored!");
-        	gamestate.getMainPlayer().scorePoint();
-        	System.out.println("score: "+gamestate.getMainPlayer().getScore());
+        	gameState.getMainPlayer().scorePoint();
+        	System.out.println("score: "+gameState.getMainPlayer().getScore());
         }
-        assertTrue(achievement.isUnlocked(gamestate));
-        assertFalse(achievement2.isUnlocked(gamestate));
+        assertTrue(achievement.isUnlocked(gameState));
+        assertFalse(achievement2.isUnlocked(gameState));
 	}
 
 }
