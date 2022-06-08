35b3e289fb591a25743e9881813dd5d404765107
Francesca Lanzi <francesca.lanzi6@studio.unibo.it>
2022-06-08 10:07:43 +0200

    Update AchievementTest

--- src/tests/AchievementTest.java
+++ src/tests/AchievementTest.java
@@ -10,8 +10,8 @@ public class AchievementTest {
     @org.junit.Test
 	public void testAchievement() {
 		var gameState = new GameStateBuilder().build();
-        var achievement = new AchievementImpl("", "Champion", "Score 5 goals in one game", (game) -> game.getMainPlayer().getScore() >= 5);
-        var achievement2 = new AchievementImpl("", "PogChamp", "score 15 goals in one game", (game) -> game.getMainPlayer().getScore() >= 15);
+        var achievement = new AchievementImpl("Champion", "Score 5 goals in one game", (game) -> game.getMainPlayer().getScore() >= 5);
+        var achievement2 = new AchievementImpl("PogChamp", "score 15 goals in one game", (game) -> game.getMainPlayer().getScore() >= 15);
         
         System.out.println("initial score: "+gameState.getMainPlayer().getScore());
         
