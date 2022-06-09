43f0ec3f6f0bb97db2f91a73be5b4d7fa0739995
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-06-09 22:06:16 +0200

    Update Theme enum  and methods

--- src/gui/Theme.java
+++ src/gui/Theme.java
@@ -1,8 +1,21 @@
 package gui;
 
+import java.awt.Color;
+
 public enum Theme {
-	RED,
-	GREEN,
-	BLUE,
-	BLACK
+	DEFAULT(new Color(0, 0, 0, 0)),
+	RED(new Color(255, 0, 0, 100)),
+	GREEN(new Color(0, 255, 0, 100)),
+	BLUE(new Color(0, 0, 255, 100)),
+	BLACK(new Color(0, 0, 0, 100));
+	
+	private Color color;
+	
+	Theme(Color color) {
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
 }
