8d255417f823a1dff58abf90c52142bf23284ef3
Edoardo La Greca <elagreca@protonmail.com>
2022-06-11 19:29:08 +0200

    Turn some constructor variables into private class fields

--- src/gui/AchievementLabel.java
+++ src/gui/AchievementLabel.java
@@ -9,6 +9,11 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 
 public class AchievementLabel extends JLabel {
+
+    private final JLabel iconLabel;
+    private final JLabel nameLabel;
+    private final JLabel descriptionLabel;
+
     public AchievementLabel(GameState game, Achievement achievement) {
         this.setLayout(new GridBagLayout());
 
@@ -17,20 +22,20 @@ public class AchievementLabel extends JLabel {
         if (!achievement.isUnlocked(game)) {
             icon = new ImageIcon(ImageModifier.color(icon.getImage(), Color.GRAY));
         }
-        JLabel iconLabel = new JLabel(icon);
-        JLabel nameLabel = new JLabel(achievement.getName());
-        JLabel descriptionLabel = new JLabel(achievement.getDescr());
+        this.iconLabel = new JLabel(icon);
+        this.nameLabel = new JLabel(achievement.getName());
+        this.descriptionLabel = new JLabel(achievement.getDescr());
         // Add Components to the label
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
         c.gridheight = 2;
-        this.add(iconLabel, c);
+        this.add(this.iconLabel, c);
         c.gridx = 1;
         c.gridy = 0;
         c.gridheight = 1;
-        this.add(nameLabel, c);
+        this.add(this.nameLabel, c);
         c.gridy = 1;
-        this.add(descriptionLabel, c);
+        this.add(this.descriptionLabel, c);
     }
 }
