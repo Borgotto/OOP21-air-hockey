2a670669309c6c99b3de97af0766382a5dc69e41
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-13 13:08:14 +0200

    Revert small change from commit 161cbba
    Commit's SHA:
    161cbba9fb978fe16659c9d5fa21fd9fb35ec6c7
    Reverted line 52 of PausePanel.java

--- src/gui/PausePanel.java
+++ src/gui/PausePanel.java
@@ -42,6 +42,7 @@ public class PausePanel extends AbstractGridBagLayoutJComponent {
             } catch (IOException ex) {
                 new ExceptionPanel(ex);
             }
+            System.exit(0);
         });
         this.add(saveAndQuit, c);
     }
