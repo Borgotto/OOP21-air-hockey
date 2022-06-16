bf0b544a76b57a05479bacde3c51ff0128159f0c
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-16 15:16:12 +0200

    Remove print to std out from AchievementTest

--- src/tests/AchievementTest.java
+++ src/tests/AchievementTest.java
@@ -12,13 +12,9 @@ public class AchievementTest {
 		var gameState = new GameStateBuilder().build();
         var achievement = new AchievementImpl("Champion", "Score 5 goals in one game", (game) -> game.getMainPlayer().getScore() >= 5);
         var achievement2 = new AchievementImpl("PogChamp", "score 15 goals in one game", (game) -> game.getMainPlayer().getScore() >= 15);
-        
-        System.out.println("initial score: "+gameState.getMainPlayer().getScore());
-        
+                
         for(int i=0;i<10;i++) {
-        	System.out.println("\n\nscored!");
         	gameState.getMainPlayer().scorePoint();
-        	System.out.println("score: "+gameState.getMainPlayer().getScore());
         }
         assertTrue(achievement.isUnlocked(gameState));
         assertFalse(achievement2.isUnlocked(gameState));
