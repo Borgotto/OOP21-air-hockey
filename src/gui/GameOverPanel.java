02e6f76b1da9167e2bc10a0d50457a7c62af123a
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-19 18:59:42 +0200

    Add padding to GameOverPanel

--- src/gui/GameOverPanel.java
+++ src/gui/GameOverPanel.java
@@ -31,7 +31,9 @@ public class GameOverPanel extends AbstractGridBagLayoutJPanel {
             System.exit(0);
         });
         // Add all components to this panel
+        int pad = this.getPreferredSize().height/40;
         c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(pad, pad, pad, pad);
         c.gridwidth = 2;
         c.gridx = 0;
         c.gridy = 0;
@@ -39,7 +41,9 @@ public class GameOverPanel extends AbstractGridBagLayoutJPanel {
         c.gridy = 1;
         this.add(gameScore,c);
         c.gridy = 2;
+        c.insets = new Insets(0,0,0,0);
         this.add(achPanel,c);
+        c.insets = new Insets(pad, pad, pad, pad);
         c.gridwidth = 1;
         c.gridy = 3;
         c.weightx = 1/2d;
