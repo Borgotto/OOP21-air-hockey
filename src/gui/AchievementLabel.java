b9fd34b48ce3da52792075dfc12173fda12d3137
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-19 19:09:23 +0200

    AchievementLabel now extends JComponent
    For some reason it would throw a NullPointerException on non Windows OS if any other subclass was chosen

--- src/gui/AchievementLabel.java
+++ src/gui/AchievementLabel.java
@@ -7,7 +7,7 @@ import utils.ImageModifier;
 import javax.swing.*;
 import java.awt.*;
 
-public class AchievementLabel extends AbstractGridBagLayoutJPanel {
+public class AchievementLabel extends JComponent {
     private static final long serialVersionUID = 5723510034229737L;
     private final JLabel iconLabel;
     private final JLabel nameLabel;
