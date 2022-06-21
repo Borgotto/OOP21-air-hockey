eaff29a117838ae57966a272d9d871f3a21ba570
Emanuele Borghini <manuborg2002@gmail.com>
2022-06-21 16:38:31 +0200

    Add setDirection method and implement update method in MainPlayer

--- src/logics/MainPlayer.java
+++ src/logics/MainPlayer.java
@@ -1,9 +1,14 @@
 package logics;
 
+import org.jbox2d.common.Vec2;
+
 /**
  * Interface used to represent the main player controlled by the user.
  */
 public interface MainPlayer extends Player {
-    // TODO add methods to control the player (that aren't through the gui)
-    // not necessary for the current version of the game
+    /**
+     * Sets the direction where the player will move towards in the next update.
+     * @param direction the direction of the player, if null is passed it will stop the player.
+     */
+    void setDirection(Vec2 direction);
 }
--- src/logics/MainPlayerImpl.java
+++ src/logics/MainPlayerImpl.java
@@ -2,22 +2,34 @@ package logics;
 
 import org.jbox2d.common.Vec2;
 import physics.Physics2D;
+import physics.PointPhysicsHandler;
 
 /**
  * Implementation of the MainPlayer interface.
  */
 public class MainPlayerImpl extends AbstractPlayer implements MainPlayer {
     private static final long serialVersionUID = 5200285706483970366L;
+    private final PointPhysicsHandler pointPhysicsHandler;
+    private Vec2 direction;
 
     public MainPlayerImpl(String name, float radius, Vec2 startingPosition, Physics2D physics) {
         super(name, radius, startingPosition, physics);
+        this.pointPhysicsHandler = new PointPhysicsHandler(this.getBody());
     }
 
     public MainPlayerImpl(float radius, Vec2 startingPosition, Physics2D physics) {
         this("Player", radius, startingPosition, physics);
     }
 
-    public void update() {
+    public void setDirection(Vec2 direction) {
+        this.direction = direction;
+    }
 
+    public void update() {
+        if (this.direction == null){
+            this.pointPhysicsHandler.pointReleased();
+        } else {
+            this.pointPhysicsHandler.update(this.direction);
+        }
     }
 }
