4e0c641873f6e9cb33539da405c02f0c904280cd
Francesca Lanzi <francesca.lanzi6@studio.unibo.it>
2022-06-22 12:51:32 +0200

    Add new achievements in AchievementLogicFactoryImpl

--- src/logics/AchievementLogicsFactoryImpl.java
+++ src/logics/AchievementLogicsFactoryImpl.java
@@ -2,25 +2,29 @@ package logics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AchievementLogicsFactoryImpl implements AchievementLogicsFactory {
 
     public AchievementLogics baseAchievements() {
         AchievementLogicsImpl achievementLogics = new AchievementLogicsImpl();
         ArrayList<Achievement> list = new ArrayList<>();
+        Settings settings = new Settings();
 		
-		list.add(new AchievementImpl("A piece of cake","Scored 5 goals in dumb mode", (g)-> {return g.getMainPlayer().getScore() >= 5 
-				&& g.getEnemyPlayer().getDifficulty() == Difficulty.DUMB;}));
-		list.add(new AchievementImpl("5 o'clock","Scored 5 goals in easy mode", (g)-> {return g.getMainPlayer().getScore() >= 5 
-				&& g.getEnemyPlayer().getDifficulty() == Difficulty.EASY;}));
-		list.add(new AchievementImpl("You got some skills","Scored 5 goals in normal mode", (g) -> {return g.getMainPlayer().getScore() >= 5 
-				&& g.getEnemyPlayer().getDifficulty() == Difficulty.NORMAL;}));
-		list.add(new AchievementImpl("Building Confidence", "Scored 10 goals in dumb mode", (g)-> {return g.getMainPlayer().getScore() >= 10 
-				&& g.getEnemyPlayer().getDifficulty() == Difficulty.DUMB;}));
-		list.add(new AchievementImpl("AirHockey Master","Scored 10 goals in easy mode", (g)-> {return g.getMainPlayer().getScore() >= 10 
-				&& g.getEnemyPlayer().getDifficulty() == Difficulty.EASY;}));
-		list.add(new AchievementImpl("AirHockey Lord","Scored 10 goals in normal mode", (g)-> {return g.getMainPlayer().getScore() >= 10 
-				&& g.getEnemyPlayer().getDifficulty() == Difficulty.NORMAL;})); 
+		list.add(new AchievementImpl("A piece of cake","Score at least 5 goals in dumb mode", (g)->  (g.getMainPlayer().getScore() >= 5 
+				&& g.getEnemyPlayer().getDifficulty() == Difficulty.DUMB)));
+		list.add(new AchievementImpl("5 o'clock","Score at least 5 goals in easy mode", (g)->  (g.getMainPlayer().getScore() >= 5 
+				&& g.getEnemyPlayer().getDifficulty() == Difficulty.EASY)));
+		list.add(new AchievementImpl("Building Confidence", "Score at least 10 goals in normal mode", (g)->  (g.getMainPlayer().getScore() >= 10 
+				&& g.getEnemyPlayer().getDifficulty() == Difficulty.NORMAL)));
+		list.add(new AchievementImpl("AirHockey Lord","Score at least 10 goals in insane mode", (g)-> (g.getMainPlayer().getScore() >= 10 
+				&& g.getEnemyPlayer().getDifficulty() == Difficulty.INSANE)));
+		list.add(new AchievementImpl("The Gleam Eyes", "Win a game without enemy's goals", (g) -> (g.getWinner().equals(Optional.of(g.getMainPlayer()))
+				&& g.getEnemyPlayer().getScore() == 0)));
+		list.add(new AchievementImpl("Girlfriend (Seems easy, it's nearly impossible)", "The enemy player win in dumb mode", (g) -> (g.getWinner().get() == g.getEnemyPlayer()
+				&& g.getEnemyPlayer().getDifficulty() == Difficulty.DUMB)));
+		list.add(new AchievementImpl("Asuka Red", "The theme is set to RED", (g) -> settings.load().getTheme() == Theme.RED));
+		list.add(new AchievementImpl("I'm Blue", "The theme is set to BLUE", (g) -> settings.load().getTheme() == Theme.BLUE));
 		achievementLogics.addAchievements(list);
         return achievementLogics;
     }
